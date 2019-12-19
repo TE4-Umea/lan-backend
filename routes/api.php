8b2465bb2a7110cb5f43c9f9667043853890f8de
Elias Renman <33152039+Abborren@users.noreply.github.com>
2019-12-19 14:53:26 +0100

    Feature elias renman passport auth (#1)
    * Regular login now works
    
    * removed cors protection
    
    * changed user data

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,7 +15,18 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/', function() {
+    return '';
+});
+Route::prefix('/auth/')->group(function () {
+    
+    Route::middleware('auth:api')->group(function () { 
+        Route::get('/user', function (Request $request) {
+            return ["user" => $request->user()];
+        });
+        Route::post('/logout', 'AuthController@logout')->name('auth.logout');
+    });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::post('login', 'AuthController@login')->name('auth.login');
+    Route::post('register', 'AuthController@register')->name('auth.register');
 });

a5f3848fb5b1850cb8209b6999d1b590e4603b83
Elias Renman <elias.renman00@gmail.com>
2019-12-19 13:34:16 +0100

    added cors protection

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,13 +18,14 @@ use Illuminate\Http\Request;
 Route::get('/', function() {
     return '';
 });
-Route::middleware('auth:api')->group(function () { 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-    Route::post('/auth/logout', 'AuthController@logout')->name('auth.logout');
-});
 Route::prefix('/auth/')->group(function () {
+    
+    Route::middleware('auth:api')->group(function () { 
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::post('/logout', 'AuthController@logout')->name('auth.logout');
+    });
 
     Route::post('login', 'AuthController@login')->name('auth.login');
     Route::post('register', 'AuthController@register')->name('auth.register');

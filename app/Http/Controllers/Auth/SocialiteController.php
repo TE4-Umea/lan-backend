a6d1bec31ff73f0233461899e9bc1b0a03b2757f
Elias Renman <elias.renman00@gmail.com>
2019-12-23 13:39:07 +0100

    socialite callback works and creates users

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Socialite;
+use App\User;
 
 class SocialiteController extends Controller
 {
@@ -25,7 +29,27 @@ class SocialiteController extends Controller
     * @return \Illuminate\Http\Response
     */public function handleProviderCallback($provider)
     {
-        $user = Socialite::driver($provider)->stateless()->user();    // return the Laravel Passport access token response
-        dd([$user, $provider]);
+        $social_user = Socialite::driver($provider)->stateless()->user();    // return the Laravel Passport access token response
+        // dd([$social_user, $provider]);
+        $this->findOrCreate($social_user);
+        return [
+            "token" => $social_user->token,
+            "refreshToken" => $social_user->refreshToken,
+            "accessType" => $provider,
+        ];
+    }
+    public function findOrCreate($social_user)
+    {
+        try {
+            User::firstOrFail()->where('email', $social_user->email);
+        } catch (ModelNotFoundException $e) {
+            // dd(["This user does not exist", $social_user]);
+            User::create([
+                "name" => $social_user->name,
+                "email" => $social_user->email,
+                "password" =>  Hash::make(Str::random(52)),
+                "email_verified_at" => now()
+            ]);
+        }
     }
 }

45abb6327427455453163751aa46bb99ae28e49e
Edbergarn <Edbergarn@gmail.com>
2020-01-14 12:47:02 +0100

    added migrations (#4)

---
+++ database/migrations/2020_01_13_082036_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 32);
+            $table->string('short_info', 32);
+            $table->integer('rules_id');
+            $table->timestamp('start_date', 0)->nullable();
+            $table->timestamp('end_date', 0)->nullable();
+            $table->timestamp('registration_closes_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}

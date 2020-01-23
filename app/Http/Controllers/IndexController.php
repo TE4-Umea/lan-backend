a85a53d54fd80285b492145949368c3c204541b2
Elias Renman <elias.renman00@gmail.com>
2020-01-23 09:31:44 +0100

    removed test

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,14 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\RegistrationChange;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        RegistrationChange::dispatch(22);
-        return 'asd';
+        return '';
     }
 }

82f7d8bf05f6425dc49e2e857683df4dcc76bf2b
Elias Renman <elias.renman00@gmail.com>
2020-02-11 11:08:12 +0100

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceSchema('https');
+        URL::forceScheme('https');
         Schema::defaultStringLength(191);
     }
 }

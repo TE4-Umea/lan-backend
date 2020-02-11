5f66e823e80f145a5109d2b377f25e22e4b38062
Elias Renman <elias.renman00@gmail.com>
2020-02-11 10:48:48 +0100

    wip

--- app/Http/Controllers/Auth/PassportAuthController.php
+++ app/Http/Controllers/Auth/PassportAuthController.php
@@ -25,7 +25,6 @@ class PassportAuthController extends Controller
             ]);
             return $response->getBody();
         } catch (\GuzzleHttp\Exception\BadResponseException $e) {
-            dd($e);
             if ($e->getCode() === 400) {
                 return response()->json('Invalid Request. Please enter a email or a password.', $e->getCode());
             } else if ($e->getCode() === 401) {
